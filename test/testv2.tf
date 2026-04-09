646a4fb12b12c9d772d6437f9299a6f4f5cc8215
Simonrun1 <simon.2009@live.se>
2026-04-09 10:17:04 +0200

    Update testv2.tf

--- test/testv2.tf
+++ test/testv2.tf
@@ -14,7 +14,7 @@ resource "proxmox_vm_qemu" "test_vm" {
   name        = "terraform-test"
   target_node = "proxmox"
 
-  clone = "ubuntu-22.04-template"
+  clone = "ubuntu-2404-q35-template"
 
   cores  = 1
   memory = 1024
