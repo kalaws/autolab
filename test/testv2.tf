5e821961b4f8c4bff718acc6eb7c56ec8fdc9175
Simonrun1 <simon.2009@live.se>
2026-04-09 10:11:26 +0200

    Update testv2.tf

--- test/testv2.tf
+++ test/testv2.tf
@@ -1,8 +1,5 @@
 provider "proxmox" {
-  pm_api_url      = "https://100.90.144.70:8006/api2/json"
-  pm_user         = "root@pam"
-  pm_password     = "rootpam"
-  pm_tls_insecure = true
+
 }
 
 resource "proxmox_vm_qemu" "test_vm" {
