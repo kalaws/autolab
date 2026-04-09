4432e5479112ba910813d80d17f85dbc4f36e201
Simonrun1 <simon.2009@live.se>
2026-04-09 10:44:41 +0200

    Update testv2.tf

--- test/testv2.tf
+++ test/testv2.tf
@@ -12,7 +12,7 @@ provider "proxmox" {}
 
 resource "proxmox_vm_qemu" "test_vm" {
   name        = "terraform-test"
-  target_node = "pve"
+  node_name = "pve"
 
   clone {
     vm_id = "ubuntu-2404-q35-template"
