e8e7699e804b9254e3816d8c4d7feb090de2c9cb
kalaws <beta_granit_1l@icloud.com>
2026-04-15 16:15:04 +0200

    fix locals variable to print correct output

--- kubernetes-lab/main.tf
+++ kubernetes-lab/main.tf
@@ -133,10 +133,10 @@ resource "proxmox_virtual_environment_container" "ansible" {
 
 locals {
   control_ip = proxmox_virtual_environment_container.ansible.ipv4["eth0"]
-  target_ips = {
-    for name, vm in proxmox_virtual_environment_vm.k8s_control :
-    name => vm.ipv4_addresses[1]
-  }
+  target_ips = merge(
+    { "control" = proxmox_virtual_environment_vm.k8s_control.ipv4_addresses[1][0] },
+    { for name, vm in proxmox_virtual_environment_vm.k8s_worker : name => vm.ipv4_addresses[1][0] }
+  )
 }
 
 # ============================================
