64cfb27964cdba3fcaaa212b05cc937da4bcdafb
Simonrun1 <simon.2009@live.se>
2026-04-15 15:20:48 +0200

    ändrade namn på outputs samt container till vm

--- kubernetes-lab/outputs.tf
+++ kubernetes-lab/outputs.tf
@@ -1,10 +1,10 @@
-output "ansible_control_ip" {
+output "ansible" {
   value = try(proxmox_virtual_environment_container.ansible.ipv4["eth0"], "not available yet")
 }
 
-output "ansible_target_ips" {
+output "k8s_control" {
   value = {
-    for name, ct in proxmox_virtual_environment_container.k8s_control :
+    for name, ct in proxmox_virtual_environment_vm.k8s_control :
     name => try(ct.ipv4["eth0"], "not available yet")
   }
 }
