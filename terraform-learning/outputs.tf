6e0beb39b99fdccc5ea2a7bdad3df9b203611054
Simonrun1 <simon.2009@live.se>
2026-04-27 20:10:16 +0200

    fix outputs.tf

--- terraform-learning/outputs.tf
+++ terraform-learning/outputs.tf
@@ -1,9 +1,9 @@
 output "vm_id" {
     description = "ID för den skapade VMen"
-    value = proxmox_virtual_environment_vm.learning_vm.vm_id
+    value       = proxmox_virtual_environment_vm.learning_vm.vm_id
 }
 
 output "vm_name" {
-    description = "Namnet på den skapde VMen"
-    value = proxmox_virtual_environment_vm.learning_vm.vm_name
+  description = "Namnet på den skapade VMen"
+  value       = proxmox_virtual_environment_vm.learning_vm.name
 }
