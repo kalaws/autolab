d6b6255bfda0a7b80a795380f38bddb3592ea19e
Simonrun1 <simon.2009@live.se>
2026-05-01 14:00:24 +0200

    fix i outputs

--- terraform-learning/modules/virtual-machine/outputs.tf
+++ terraform-learning/modules/virtual-machine/outputs.tf
@@ -5,5 +5,5 @@ output "vm_id" {
 
 output "vm_name" {
     description = "Namnet på den skapade VMen"
-    value = proxmox_virtual_environment_vm.vm.vm_name
+    value = proxmox_virtual_environment_vm.vm.vm.name
 }
