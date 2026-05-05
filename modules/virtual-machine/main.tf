5bff946f126e61d4047495df97dbbe758fd1088e
kalaws <beta_granit_1l@icloud.com>
2026-05-05 15:43:30 +0200

    modules: add vm

--- modules/virtual-machine/main.tf
+++ modules/virtual-machine/main.tf
@@ -1,19 +1,55 @@
-terraform {
-  required_providers {
-    proxmox = {
-      source  = "bpg/proxmox"
-      version = "~> 0.100.0"
-    }
-  }
-}
-
-# ============================================
-# 1. Slå upp Packer-byggt template
-# ============================================
 data "proxmox_virtual_environment_vms" "packer_template" {
-  node_name = "pve"
+  node_name = var.node_name
   filter {
     name   = "name"
     values = var.packer_template
+  }
+}
+
+resource "proxmox_virtual_environment_vm" "vm" {
+  name      = var.vm_name
+  node_name = var.node_name
+
+  clone {
+    vm_id = data.proxmox_virtual_environment_vms.packer_template.vms[0].vm_id
+  }
+
+  memory {
+    dedicated = var.memory
+  }
+
+  cpu {
+    cores = var.cpu_cores
+  }
+
+  disk {
+    datastore_id = "local-lvm"
+    interface    = "virtio0"
+    iothread     = true
+    discard      = "on"
+    size         = var.disk
+  }
+
+  network_device {
+    bridge = var.bridge_wan
+  }
+
+  initialization {
+    ip_config {
+      ipv4 {
+        address = "dhcp"
+      }
+    }
+
+    user_account {
+      username = var.ansible_user
+      keys     = [var.ansible_ssh_public_key]
+    }
+  }
+
+  stop_on_destroy = true
+
+  agent {
+    enabled = true
   }
 }
--- modules/virtual-machine/variables.tf
+++ modules/virtual-machine/variables.tf
@@ -1,5 +1,47 @@
+variable "vm_name" {
+  type        = string
+  description = "Namn på VM:en"
+}
+
+variable "node_name" {
+  type        = string
+  description = "Proxmox-nod att deploya på"
+  default     = "pve"
+}
+
 variable "packer_template" {
   type        = list(string)
-  description = "Packer template för kubernetes VMs"
+  description = "Packer template att klona"
   default     = ["ubuntu-2404-q35-template"]
+}
+
+variable "memory" {
+  type        = number
+  description = "RAM i MB"
+}
+
+variable "cpu_cores" {
+  type        = number
+  description = "Antal CPU-cores"
+}
+
+variable "disk" {
+  type        = number
+  description = "Diskstorlek i GB"
+}
+
+variable "bridge_wan" {
+  type        = string
+  description = "Bridge för nätverksinterface"
+}
+
+variable "ansible_user" {
+  type        = string
+  description = "Maskinanvändare som används av Ansible"
+  default     = "ansible"
+}
+
+variable "ansible_ssh_public_key" {
+  type        = string
+  description = "Publik SSH-nyckel för ansible-användaren (injiceras vid cloud-init)"
 }
