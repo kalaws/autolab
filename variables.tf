f403ecc8972fd3a18d245fb8a14750a445259d4d
Simonrun1 <simon.2009@live.se>
2026-05-05 13:13:19 +0200

    lade till variables för bootstappen

--- variables.tf
+++ variables.tf
@@ -24,3 +24,50 @@ variable "resources" {
     disk     = number  
   }))
 }
+
+variable "github_owner" {
+  type        = string
+  description = "GitHub-användarnamn eller organisation"
+  default     = "kalaws"
+}
+
+variable "github_repo" {
+  type        = string
+  description = "GitHub-repository namn"
+  default     = "autolab"
+}
+
+variable "bridge_wan" {
+  type        = string
+  description = "Bridge för WAN-nät"
+  default     = "vnet1"
+}
+
+variable "dns_servers" {
+  type        = list(string)
+  description = "DNS-servrar för CT:arna, utöver gateway"
+  default     = ["8.8.8.8"]
+}
+
+variable "workers" {
+  type        = list(string)
+  description = "Kubernetes worker nodes"
+  default     = ["1", "2"]
+}
+
+variable "ssh_public_key_path" {
+  type    = string
+  default = "~/.ssh/id_ed25519.pub"
+}
+
+variable "terraform_ssh_user" {
+  type        = string
+  description = "Dedikerad Terraform-användare på ansible control node"
+  default     = "terraform"
+}
+
+variable "ansible_user" {
+  type        = string
+  description = "Ansible-användare på k8s-noder"
+  default     = "ansible"
+}
