ddbc2f114a3fa1c2f1e8b243d0208f4793cfe594
kalaws <beta_granit_1l@icloud.com>
2026-05-05 16:59:06 +0200

    rename vm/cts

--- variables.tf
+++ variables.tf
@@ -13,9 +13,9 @@ variable "ct_template" {
 variable "resources" {   
   description = "VM/CT configurations for Kubernetes lab"   
   default = {     
-    ansible  = { hostname = "LAB-K8S-ansible", cores = 1, memory = 512, disk = 8 }
-    k8s_control  = { hostname = "LAB-K8S-master", cores = 2, memory = 4096,  disk = 20 }          
-    k8s_worker  = { hostname = "LAB-K8S-worker", cores = 2, memory = 4096, disk = 40 }
+    ansible  = { hostname = "LABITS-ansible", cores = 1, memory = 512, disk = 8 }
+    k8s_control  = { hostname = "LABITS-K8S-master", cores = 2, memory = 4096,  disk = 20 }          
+    k8s_worker  = { hostname = "LABITS-K8S-worker", cores = 2, memory = 4096, disk = 40 }
   } 
   type = map(object({  
     hostname = string   
