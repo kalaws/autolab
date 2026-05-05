e69d01371958f82927d6fb03c406e406d97fdb8f
kalaws <beta_granit_1l@icloud.com>
2026-05-05 12:31:49 +0200

    init: provisioners

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "bpg/proxmox"
+      version = "0.100.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.0"
+    }
+  }
+}
+
+provider "proxmox" {}
