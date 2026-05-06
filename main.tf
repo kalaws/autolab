20f840a8fd248219f0ceb8518dc70751dfe47465
kalaws <beta_granit_1l@icloud.com>
2026-05-06 16:21:00 +0200

    vault: correct formatting to hand off shell command

--- main.tf
+++ main.tf
@@ -231,7 +231,7 @@ resource "terraform_data" "bootstrap_vault" {
         apt-get update -qq
         apt-get install -y gpg curl
         curl -fsSL https://apt.releases.hashicorp.com/gpg | gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
-        echo 'deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com \$(lsb_release -cs) main' | tee /etc/apt/sources.list.d/hashicorp.list
+        echo \"deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com \$(lsb_release -cs) main\" | tee /etc/apt/sources.list.d/hashicorp.list
         apt-get update -qq
         apt-get install -y vault
         mkdir -p /opt/vault/data
