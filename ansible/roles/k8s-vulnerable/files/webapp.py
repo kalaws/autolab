adb6a4312215def21f77e140b1fa073094a536ec
kalaws <beta_granit_1l@icloud.com>
2026-05-12 20:05:14 +0200

    webapp: linebreak after welcome message

--- ansible/roles/k8s-vulnerable/files/webapp.py
+++ ansible/roles/k8s-vulnerable/files/webapp.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return f'Webapp running on {socket.gethostname()}'
+    return f'Webapp running on {socket.gethostname()}\n'
 
 @app.route('/debug')
 def debug():
