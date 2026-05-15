97fd63db7654e958f8d12895c26093e1277c5eaf
kalaws <beta_granit_1l@icloud.com>
2026-05-15 11:34:57 +0200

    visa hostname som del av output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,18 +1,27 @@
 output "ansible" {
-  value = try(module.ansible.ipv4_address, "not available yet")
+  value = {
+    hostname = "${var.resources["ansible"].hostname}.${var.domain}"
+    ip       = try(module.ansible.ipv4_address, "not available yet")
+  }
 }
 
 output "vault" {
-  value = try(module.vault.ipv4_address, "not available yet")
+  value = {
+    hostname = "${var.resources["vault"].hostname}.${var.domain}"
+    ip       = try(module.vault.ipv4_address, "not available yet")
+  }
 }
 
 output "k8s_control" {
-  value = try(module.k8s_control.ipv4_address, "not available yet")
+  value = {
+    hostname = "${var.resources["k8s_control"].hostname}.${var.domain}"
+    ip       = try(module.k8s_control.ipv4_address, "not available yet")
+  }
 }
 
 output "k8s_workers" {
-  value = [
-    for vm in module.k8s_worker :
-    try(vm.ipv4_address, "not available yet")
-  ]
+  value = {
+    for w in var.workers :
+    "${var.resources["k8s_worker"].hostname}-${w}.${var.domain}" => try(module.k8s_worker[w].ipv4_address, "not available yet")
+  }
 }
